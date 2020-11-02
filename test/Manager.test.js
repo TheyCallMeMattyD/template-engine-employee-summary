86382f8807754b841b7846a56257c78d1069a59b
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-02 12:24:03 -0500

    added testing script to json and fixed managerTest typo

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,20 +1,20 @@
-const namager = require("../lib/manager");
+const manager = require("../lib/manager");
 const employee = require("../lib/employee");
 
 test("Can set office number via constructor argument", () => {
     const testValue = 100;
-    const e = new namager("Foo", 1, "test@test.com", testValue);
+    const e = new manager("Foo", 1, "test@test.com", testValue);
     expect(e.officeNumber).toBe(testValue);
 });
 
-test('getRole() should return "namager"', () => {
-    const testValue = "namager";
-    const e = new namager("Foo", 1, "test@test.com", 100);
+test('getRole() should return "manager"', () => {
+    const testValue = "manager";
+    const e = new manager("Foo", 1, "test@test.com", 100);
     expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get office number via getOffice()", () => {
     const testValue = 100;
-    const e = new namager("Foo", 1, "test@test.com", testValue);
+    const e = new manager("Foo", 1, "test@test.com", testValue);
     expect(e.getOfficeNumber()).toBe(testValue);
 });
